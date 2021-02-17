4a74020b6b9e1aa25402ca40c827e9d53ea08e2d
houshmandX <houshmand.esmaeilpour@gmail.com>
2021-02-16 16:57:05 -0800

    added irst part of lab2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -2,8 +2,8 @@
 #include "user.h"
 
 int PScheduler(void);
-int donationScheduler(int value);
-int waitScheduler(void);
+//int donationScheduler(int value);
+//int waitScheduler(void);
 
 int main(int argc, char *argv[])
 {
@@ -15,11 +15,11 @@ int main(int argc, char *argv[])
   if (atoi(argv[1]) == 1){
     printf(1, "\n Testing Scheduler\n");
     PScheduler();
-  } else if (atoi(argv[1]) == 2){
-    printf(1, "\n Testing Donation\n");
-    donationScheduler(3); //donation amount
+  // } else if (atoi(argv[1]) == 2){
+  //   printf(1, "\n Testing Donation\n");
+  //   donationScheduler(3); //donation amount
+  // }
   }
-
   exit();
 }
 
@@ -65,90 +65,90 @@ int PScheduler(void){
   return 0;
 }
 
-int donationScheduler(int value){
- int parent, pid, i, j;
+// int donationScheduler(int value){
+//  int parent, pid, i, j;
 
- printf(1, "\n Step 1: Testing the donate systema call:\n");
- printf(1, " Step 2: The parent will have a higher priority than the child\n");
- printf(1, " Step 3: The parent will donate the value amount of priority to the child\n");
- printf(1, " Step 4: The priority of the child will go up by the same amount as the parent will go down\n");
- set_priority(getpid(), 20);
- parent = getpid();
- printf(1, "\n Child with pid %d has priority %d \n ", getpid(), get_priority(getpid()) );
- pid = fork();
-
-
- if(pid > 0){
-   wait();
-   printf(1, "\n Child with pid %d now has priority %d after donation \n", getpid(), get_priority(getpid()) );
- }
- else if(pid == 0){
-   set_priority(getpid(), 10);
-   printf(1, " Parent with pid %d has priority %d \n ", getpid(), get_priority(getpid()) );
-   for (i=0;i<50000;i++) {
-     for(j=0;j<10000;j++) {
-       asm("nop");
-     }
-   }
-   printf(1, "\n Parent with pid %d has donated %d priority to child %d \n", getpid(), value, parent);
-   //printf(1, "\n Parent with pid %d has donated %d priority to child %d \n", parent, value, getpid());
-   donate_priority(parent, value);
-   for (i=0;i<50000;i++) {
-     for(j=0;j<10000;j++) {
-       asm("nop");
-     }
-   }
-   printf(1, "\n Parent with pid %d now has priority %d after donation \n", getpid(), get_priority(getpid()) );
-   exit();
- }
- else{
-   printf(2, "\n Error \n ");
- }
-
- return 0;
-}
-
-int waitScheduler(void){
- int pid, i, j;
-
- printf(1, "\n  Step 1: Testing the donate systema call:\n");
- printf(1, "  Step 2: The parent will have a higher priority than the child\n");
- printf(1, "  Step 3: The parent will donate the value amount of priority to the child\n");
- printf(1, "  Step 4: The priority of the child will go up by the same amount as the parent will go down\n");
- set_priority(getpid(), 15);
- printf(1, "\n  Parent with pid %d has priority %d \n ", getpid(), get_priority(getpid()) );
- pid = fork();
+//  printf(1, "\n Step 1: Testing the donate systema call:\n");
+//  printf(1, " Step 2: The parent will have a higher priority than the child\n");
+//  printf(1, " Step 3: The parent will donate the value amount of priority to the child\n");
+//  printf(1, " Step 4: The priority of the child will go up by the same amount as the parent will go down\n");
+//  set_priority(getpid(), 20);
+//  parent = getpid();
+//  printf(1, "\n Child with pid %d has priority %d \n ", getpid(), get_priority(getpid()) );
+//  pid = fork();
 
 
- if(pid > 0){
-   wait();
-   printf(1, "  Parent with pid %d now has priority %d after donation \n", getpid(), get_priority(getpid()) );
-   printf(1, "\n child# %d wait time is %d \n",getpid(), get_wait_time(getpid()));
-   printf(1, " child# %d runtime time is %d \n",getpid(), get_runtime(getpid()));
-   printf(1, " child# %d turnaround time is %d \n",getpid(), get_turnaround_time(getpid()));
-   printf(1, " child# %d with starting priority %d and finishing priority %d has finished! \n",getpid(),10, get_priority(getpid()));
- }
- else if(pid == 0){
-   set_priority(getpid(), 10);
-   for (i=0;i<50000;i++) {
-     for(j=0;j<10000;j++) {
-       asm("nop");
-     }
-   }
-   for (i=0;i<50000;i++) {
-     for(j=0;j<10000;j++) {
-       asm("nop");
-     }
-   }
-   printf(1, "\n child# %d wait time is %d \n",getpid(), get_wait_time(getpid()));
-   printf(1, " child# %d runtime time is %d \n",getpid(), get_runtime(getpid()));
-   printf(1, " child# %d turnaround time is %d \n",getpid(), get_turnaround_time(getpid()));
-   printf(1, " child# %d with starting priority %d and finishing priority %d has finished! \n",getpid(),15, get_priority(getpid()));
-   exit();
- }
- else{
-   printf(2, "\n Error \n ");
- }
+//  if(pid > 0){
+//    wait();
+//    printf(1, "\n Child with pid %d now has priority %d after donation \n", getpid(), get_priority(getpid()) );
+//  }
+//  else if(pid == 0){
+//    set_priority(getpid(), 10);
+//    printf(1, " Parent with pid %d has priority %d \n ", getpid(), get_priority(getpid()) );
+//    for (i=0;i<50000;i++) {
+//      for(j=0;j<10000;j++) {
+//        asm("nop");
+//      }
+//    }
+//    printf(1, "\n Parent with pid %d has donated %d priority to child %d \n", getpid(), value, parent);
+//    //printf(1, "\n Parent with pid %d has donated %d priority to child %d \n", parent, value, getpid());
+//    donate_priority(parent, value);
+//    for (i=0;i<50000;i++) {
+//      for(j=0;j<10000;j++) {
+//        asm("nop");
+//      }
+//    }
+//    printf(1, "\n Parent with pid %d now has priority %d after donation \n", getpid(), get_priority(getpid()) );
+//    exit();
+//  }
+//  else{
+//    printf(2, "\n Error \n ");
+//  }
 
- return 0;
-}
+//  return 0;
+// }
+
+// int waitScheduler(void){
+//  int pid, i, j;
+
+//  printf(1, "\n  Step 1: Testing the donate systema call:\n");
+//  printf(1, "  Step 2: The parent will have a higher priority than the child\n");
+//  printf(1, "  Step 3: The parent will donate the value amount of priority to the child\n");
+//  printf(1, "  Step 4: The priority of the child will go up by the same amount as the parent will go down\n");
+//  set_priority(getpid(), 15);
+//  printf(1, "\n  Parent with pid %d has priority %d \n ", getpid(), get_priority(getpid()) );
+//  pid = fork();
+
+
+//  if(pid > 0){
+//    wait();
+//    printf(1, "  Parent with pid %d now has priority %d after donation \n", getpid(), get_priority(getpid()) );
+//    printf(1, "\n child# %d wait time is %d \n",getpid(), get_wait_time(getpid()));
+//    printf(1, " child# %d runtime time is %d \n",getpid(), get_runtime(getpid()));
+//    printf(1, " child# %d turnaround time is %d \n",getpid(), get_turnaround_time(getpid()));
+//    printf(1, " child# %d with starting priority %d and finishing priority %d has finished! \n",getpid(),10, get_priority(getpid()));
+//  }
+//  else if(pid == 0){
+//    set_priority(getpid(), 10);
+//    for (i=0;i<50000;i++) {
+//      for(j=0;j<10000;j++) {
+//        asm("nop");
+//      }
+//    }
+//    for (i=0;i<50000;i++) {
+//      for(j=0;j<10000;j++) {
+//        asm("nop");
+//      }
+//    }
+//    printf(1, "\n child# %d wait time is %d \n",getpid(), get_wait_time(getpid()));
+//    printf(1, " child# %d runtime time is %d \n",getpid(), get_runtime(getpid()));
+//    printf(1, " child# %d turnaround time is %d \n",getpid(), get_turnaround_time(getpid()));
+//    printf(1, " child# %d with starting priority %d and finishing priority %d has finished! \n",getpid(),15, get_priority(getpid()));
+//    exit();
+//  }
+//  else{
+//    printf(2, "\n Error \n ");
+//  }
+
+//  return 0;
+// }
