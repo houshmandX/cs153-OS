09a62551440cb9f028bb8a8c05df8e44901ec900
houshmandX <houshmand.esmaeilpour@gmail.com>
2021-02-19 15:44:56 -0800

    changed setprority

--- lab2/lab2_test.c
+++ lab2/lab2_test.c
@@ -72,7 +72,7 @@ int AGcheduler(void){
     // 0 is the highest priority and 31 is the lowest priority.  
 
   int pid;
-  int i,j,k;
+  int i,j,k,a,b;
   
     printf(1, "\n  Step 2: testing the priority scheduler and setpriority(int priority)) systema call:\n");
     printf(1, "\n  Step 2: Assuming that the priorities range between range between 0 to 31\n");
@@ -84,13 +84,14 @@ int AGcheduler(void){
 	if (pid > 0 ) {
 		continue;}
 	else if ( pid == 0) {
-		int myp = set_priority(30-10*i);
+		set_priority(30-10*i);
 		//set_priority(30-10*i);
-
-		for (j=0;j<90000;j++) {
-			for(k=0;k<5000;k++) {
-				asm("nop"); }}
-		printf(1, "\n child# %d with priority %d has finished! \n",getpid(),myp);		
+        for (a=0;a<500000;a++) {
+			for(b=0;b<10000;b++) {
+		for (j=0;j<50000;j++) {
+			for(k=0;k<1000;k++) {
+				asm("nop"); }}}}
+		printf(1, "\n child# %d with priority %d has finished! \n",getpid(),get_priority());		
 		exit();
         }
         else {
